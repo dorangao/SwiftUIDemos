3621866fa2fb3fe9ec355e5ea696114d3b2b4e21
Doran Gao <dorangao@gmail.com>
2024-08-04 21:09:19 -0700

    Initial Commit

---
+++ SwiftUIStateManagementDemo/SwiftUIStateManagementDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateManagementDemoApp.swift
+//  SwiftUIStateManagementDemo
+//
+//  Created by Doran Gao on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateManagementDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
